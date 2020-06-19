aff265007e1b65d5c8d2e3751e50ed2f630a0867
Luis Galaz <lgalaz@gmail.com>
2020-06-18 19:04:29 -0700

    add bootstrap vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,15 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -7,6 +7,21 @@
 
                     <div class="card-body">
                         I'm an example component.
+                         <b-card
+                            title="Card Title"
+                            img-src="https://picsum.photos/600/300/?image=25"
+                            img-alt="Image"
+                            img-top
+                            tag="article"
+                            style="max-width: 20rem;"
+                            class="mb-2"
+                        >
+    <b-card-text>
+      Luis Galaz mONTOYA
+    </b-card-text>
+
+    <b-button href="#" variant="primary">Go somewhere</b-button>
+  </b-card>
                     </div>
                 </div>
             </div>
@@ -15,6 +30,7 @@
 </template>
 
 <script>
+    import { BCard } from 'bootstrap-vue'
     export default {
         mounted() {
             console.log('Component mounted.')
